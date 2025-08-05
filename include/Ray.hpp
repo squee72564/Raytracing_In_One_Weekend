16ce7c41ab3c71133fde4f3e228329d0d17e7505
Alex Rodriguez <rodriguez.al183@gmail.com>
2025-08-04 17:34:12 -0700

    Add init raytracing example

---
+++ include/Ray.hpp
@@ -0,0 +1,23 @@
+#ifndef RAY_HPP
+#define RAY_HPP
+
+#include <glm/vec3.hpp>
+
+class Ray {
+public:
+    Ray() : origin_{}, direction_{} {}
+    Ray(glm::vec3 origin, glm::vec3 direction) : origin_{origin}, direction_{direction} {}
+
+    const glm::vec3 & origin() const noexcept { return origin_; }
+    const glm::vec3 & direction() const noexcept { return direction_; }
+
+    glm::vec3 at(float t) const {
+        return origin_ + t * direction_;
+    }
+
+private:
+    glm::vec3 origin_;
+    glm::vec3 direction_;
+};
+
+#endif // RAY-HPP
---
+++ src/main.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+
+#include "Ray.hpp"
+
+
+void write_color(std::ostream& out, const glm::vec3& color)
+{
+    auto r = color.x;
+    auto g = color.y;
+    auto b = color.z;
+
+    int rbyte = static_cast<int>(255.99 * r);
+    int gbyte = static_cast<int>(255.99 * g);
+    int bbyte = static_cast<int>(255.99 * b);
+
+
+    out << rbyte << " " << gbyte << " " << bbyte << "\n";
+}
+
+glm::vec3 ray_color(const Ray& r)
+{
+    glm::vec3 unit_direction = r.direction() / static_cast<float>(r.direction().length());
+
+    auto a = 0.5f * (unit_direction.y + 1.0f);
+
+    return (1.0f - a) * glm::vec3(1.0f, 1.0f, 1.0f) + a * glm::vec3(0.5f, 0.7f, 1.0f); 
+}
+
+int main()
+{
+    auto aspect_ratio = 16.0 / 9.0;
+    int image_width = 400;
+
+    // Calculate the image height, and ensure that it's at least 1.
+    int image_height = static_cast<int>(image_width / aspect_ratio);
+    image_height = (image_height < 1) ? 1 : image_height;
+
+    // Camera
+    auto focal_length = 1.0;
+    auto viewport_height = 2.0;
+    auto viewport_width = viewport_height * (static_cast<double>(image_width)/image_height);
+    auto camera_center = glm::vec3{0.0f, 0.0f, 0.0f};
+
+    // Calculate vectors across the horizontal and down the vertical viewport edges
+    auto viewport_u = glm::vec3(viewport_width, 0.0f, 0.0f);
+    auto viewport_v = glm::vec3(0.0f, -viewport_height, 0.0f);
+
+    // Calculate the horizontal and vertical delta vectors from pixel to pixel
+    auto pixel_delta_u = viewport_u / static_cast<float>(image_width);
+    auto pixel_delta_v = viewport_v / static_cast<float>(image_height);
+
+    auto viewport_upper_left =
+        camera_center
+        - glm::vec3(0.0f, 0.0f, focal_length)
+        - viewport_u / 2.0f
+        - viewport_v / 2.0f;
+
+    auto pixel00_loc = viewport_upper_left + 0.5f * (pixel_delta_u + pixel_delta_v);
+
+
+    // Render
+    std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
+
+    for (int i = 0; i < image_height; ++i) {
+        for (int j = 0; j < image_width; ++j) {
+            auto pixel_center =
+                pixel00_loc
+                + (static_cast<float>(j) * pixel_delta_u)
+                + (static_cast<float>(i) * pixel_delta_v);
+
+            auto ray_direction = pixel_center - camera_center;
+
+            Ray r{camera_center, ray_direction};
+
+            glm::vec3 color = ray_color(r);
+
+            write_color(std::cout, color);
+        }
+    }
+
+
+    return 0;
+}
